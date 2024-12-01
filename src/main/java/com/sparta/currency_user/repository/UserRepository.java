211011b16e6553ecf7cb92cd5dc8b0ee5d5a669c
kangjunhyuk <kangjun1290@gmail.com>
2024-12-02 04:01:18 +0900

    fix : UserRepository의 findByOrElseThrow 매서드의 오류 해결
    findById를 실행하여 같은 id가 없을경우 not fund를 반환하는
    ResponseStatusException을 발생하도록 하여 리턴값을 줌

--- src/main/java/com/sparta/currency_user/repository/UserRepository.java
+++ src/main/java/com/sparta/currency_user/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.sparta.currency_user.repository;
 
 import com.sparta.currency_user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,4 +12,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     User findByEmail(String emaill);
+
+    default User findByOrElseThrow(Long userId) {
+        return findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
 }
